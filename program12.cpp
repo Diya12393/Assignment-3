389c2fd6da0f9f7fbe106f7f966d589859367b74
Diya12393 <diyabhatt686@gmail.com>
2025-08-13 13:24:33 +0530

    Add files via upload

---
+++ program12.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+long factorial(int n);
+
+int main() {
+    int number;
+    
+    cout << "Enter a positive integer to calculate its factorial: ";
+    cin >> number;
+    
+    if (number < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << number << " = " << factorial(number) << endl;
+    }
+}
+long factorial(int n) 
+{
+       if (n == 0 || n == 1) 
+	{
+        return 1;
+    }
+    else 
+	{
+        return n * factorial(n - 1);
+    }
+}
---
+++ program16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    string reversed = str;
+    reverse(reversed.begin(), reversed.end());
+
+    if (str == reversed) {
+        cout << "The string is a palindrome.\n";
+    } else {
+        cout << "The string is not a palindrome.\n";
+    }
+}
